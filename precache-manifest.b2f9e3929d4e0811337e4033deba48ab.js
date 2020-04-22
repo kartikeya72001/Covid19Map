46b38c3cf93ab22f8368e2b5e9284ffaa9b98e15
deathcoder <kartikeya72001@gmail.com>
2020-04-22 14:01:27 +0530

    Updates

---
+++ precache-manifest.b2f9e3929d4e0811337e4033deba48ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9eb0d57283416ec9b2147f1ccf65da9",
+    "url": "/Covid19Map/index.html"
+  },
+  {
+    "revision": "64561cbd3dbcc01f9ba7",
+    "url": "/Covid19Map/static/js/2.23419492.chunk.js"
+  },
+  {
+    "revision": "b8efe049bcbcbbaef9e16e3a79454078",
+    "url": "/Covid19Map/static/js/2.23419492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fddd1c369f35f8573e95",
+    "url": "/Covid19Map/static/js/main.fd5a29bd.chunk.js"
+  },
+  {
+    "revision": "eb11898fc472c81ef9a6",
+    "url": "/Covid19Map/static/js/runtime-main.0c185bd5.js"
+  }
+]);
